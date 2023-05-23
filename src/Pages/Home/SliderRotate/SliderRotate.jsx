d32f51d9c2467921acb5b8d0e198c7f31841fb01
Sharmin64 <lizaeatsbanana@gmail.com>
2023-05-23 15:00:47 +0600

    aos implemented in the home page

--- src/Pages/Home/SliderRotate/SliderRotate.jsx
+++ src/Pages/Home/SliderRotate/SliderRotate.jsx
@@ -12,57 +12,56 @@ import extra3 from "../../../assets/extra/extra3.jpeg";
 const SliderRotate = () => {
   AOS.init({
     // Global settings:
-    disable: false,
-    startEvent: "DOMContentLoaded",
-    initClassName: "aos-init",
-    animatedClassName: "aos-animate",
-    useClassNames: false,
-    disableMutationObserver: false,
-    debounceDelay: 50,
-    throttleDelay: 99,
-
-    offset: 120,
-    delay: 0,
-    duration: 400,
-    easing: "ease",
-    once: false,
-    mirror: false,
-    anchorPlacement: "top-bottom",
+    //disable: false,
+    //startEvent: "DOMContentLoaded",
+    //initClassName: "aos-init",
+    //animatedClassName: "aos-animate",
+    //useClassNames: false,
+    //disableMutationObserver: false,
+    //debounceDelay: 50,
+    //throttleDelay: 99,
+    //offset: 120,
+    //delay: 0,
+    //duration: 1000,
+    //easing: "ease",
+    ////once: false,
+    //mirror: false,
+    //anchorPlacement: "top-bottom",
   });
 
   return (
     <>
       <div
-        data-aos="fade-up"
+        data-aos="zoom-in"
         data-aos-offset="200"
-        data-aos-delay="100"
+        data-aos-delay="50"
         data-aos-duration="1000"
         data-aos-easing="ease-in-out"
         data-aos-mirror="true"
-        //data-aos-once="false"
+        data-aos-once="false"
         data-aos-anchor-placement="top-center"
         className=" grid lg:grid-cols-3 grid-cols-1 gap-4"
       >
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={img7} alt="car!" />
+          <img className="rounded w-cover" src={img7} alt="doll" />
         </div>
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={extra3} alt="car!" />
+          <img className="rounded w-cover" src={extra3} alt="doll" />
         </div>
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={img5} alt="car!" />
+          <img className="rounded w-cover" src={img5} alt="doll" />
         </div>
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={img2} alt="car!" />
+          <img className="rounded w-cover" src={img2} alt="doll" />
         </div>
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={extra8} alt="car!" />
+          <img className="rounded w-cover" src={extra8} alt="doll" />
         </div>
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={extra9} alt="car!" />
+          <img className="rounded w-cover" src={extra9} alt="doll" />
         </div>
         <div className="card w-fit glass">
-          <img className="rounded w-cover" src={img6} alt="car!" />
+          <img className="rounded w-cover" src={img6} alt="doll" />
         </div>
       </div>
     </>

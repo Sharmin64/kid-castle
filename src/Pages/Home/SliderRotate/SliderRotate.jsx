bdf7b70a36848ff322581ecfa6a6935db4b0322c
Sharmin64 <lizaeatsbanana@gmail.com>
2023-05-23 08:04:51 +0600

    some style changed

--- src/Pages/Home/SliderRotate/SliderRotate.jsx
+++ src/Pages/Home/SliderRotate/SliderRotate.jsx
@@ -12,117 +12,56 @@ import extra3 from "../../../assets/extra/extra3.jpeg";
 const SliderRotate = () => {
   AOS.init({
     // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: "aos-init", // class applied after initialization
-    animatedClassName: "aos-animate", // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    disable: false,
+    startEvent: "DOMContentLoaded",
+    initClassName: "aos-init",
+    animatedClassName: "aos-animate",
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
 
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 400, // values from 0 to 3000, with step 50ms
-    easing: "ease", // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+    offset: 120,
+    delay: 0,
+    duration: 400,
+    easing: "ease",
+    once: false,
+    mirror: false,
+    anchorPlacement: "top-bottom",
   });
 
   return (
     <>
-      <div className=" grid lg:grid-cols-3 grid-cols-1 gap-4">
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+      <div
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="100"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        //data-aos-once="false"
+        data-aos-anchor-placement="top-center"
+        className=" grid lg:grid-cols-3 grid-cols-1 gap-4"
+      >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={img7} alt="car!" />
         </div>
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={extra3} alt="car!" />
         </div>
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={img5} alt="car!" />
         </div>
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={img2} alt="car!" />
         </div>
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={extra8} alt="car!" />
         </div>
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={extra9} alt="car!" />
         </div>
-        <div
-          data-aos="fade-up"
-          data-aos-offset="200"
-          data-aos-delay="50"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          data-aos-anchor-placement="top-center"
-          className="card w-fit glass"
-        >
+        <div className="card w-fit glass">
           <img className="rounded w-cover" src={img6} alt="car!" />
         </div>
       </div>

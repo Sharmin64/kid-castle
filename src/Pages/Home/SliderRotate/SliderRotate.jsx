f62918b4a13ac7fde25a097d28ee703364bdd179
Sharmin64 <lizaeatsbanana@gmail.com>
2024-05-01 08:09:35 +0600

    sliderotate updated

--- src/Pages/Home/SliderRotate/SliderRotate.jsx
+++ src/Pages/Home/SliderRotate/SliderRotate.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./SliderRotate.css";
 //import AOS from "aos";
 //import "aos/dist/aos.css";
-import img7 from "../../../assets/extra/img7.jpeg";
-import img6 from "../../../assets/extra/img6.jpeg";
-import img5 from "../../../assets/extra/img5.jpeg";
-import img2 from "../../../assets/extra/img2.jpeg";
-import extra8 from "../../../assets/extra/berbieBlack.jpeg";
-import extra9 from "../../../assets/extra/extra9.jpeg";
-import extra3 from "../../../assets/extra/extra3.jpeg";
+import img7 from "../../../assets/images/Cinderella_480x480.webp";
+import img6 from "../../../assets/extra/bervieWhite.jpeg";
+import img5 from "../../../assets/extra/couple.jpeg";
+import img2 from "../../../assets/extra/berStyle.jpeg";
+import extra8 from "../../../assets/extra/berbieCute.jpeg";
+//import extra9 from "../../../assets/extra/berbieBlack.jpeg";
+import extra3 from "../../../assets/images/doll_3.jpeg";
 import SectionTitle from "../../../hooks/SectionTitle";
 
 const SliderRotate = () => {
@@ -25,17 +25,78 @@ const SliderRotate = () => {
         //data-aos-mirror="true"
         //data-aos-once="false"
         //data-aos-anchor-placement="top-center"
-        className=" grid lg:grid-cols-3 grid-cols-1 md:w-full gap-4 justify-center items-center mb-10"
+        className=" grid lg:grid-cols-3 grid-cols-1 md:w-full gap-4 justify-center items-center mb-10 h-screen"
       >
-        <div className="card">
-          <div className="imgbox">
+        <div className="card relative w-[300px] h-[200px] rounded-xl shadow-xl py-8 px-10 bg-[#fff] pointer mr-8 hover:h-[420px] content-[''] translate-y-14 opacity-0 transition ease-in-out delay-150">
+          <div className="imgbox relative w-100 h-100 -translate-y-14 z-20">
             <img src={img7} alt="" />
           </div>
           <div className="content">
             <h3 className="text-pink-800">Berbie doll</h3>
-            <button className="btn px-8 py-4 bg-fuchsia-700 text-base hover:bg-fuchsia-300 hover:text-black opacity-0">
-              Buy Now
-            </button>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Obcaecati, optio.
+            </p>
+          </div>
+        </div>
+        <div className="card-box">
+          <div className="imgbox">
+            <img src={img6} alt="" />
+          </div>
+          <div className="content">
+            <h3 className="text-pink-800">Berbie doll</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Obcaecati, optio.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgbox">
+            <img src={img2} alt="" />
+          </div>
+          <div className="content">
+            <h3 className="text-pink-800">Berbie doll</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Obcaecati, optio.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgbox">
+            <img src={img5} alt="" />
+          </div>
+          <div className="content">
+            <h3 className="text-pink-800">Berbie doll</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Obcaecati, optio.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgbox">
+            <img src={extra3} alt="" />
+          </div>
+          <div className="content">
+            <h3 className="text-pink-800">Berbie doll</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Obcaecati, optio.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgbox">
+            <img src={extra8} alt="" />
+          </div>
+          <div className="content">
+            <h3 className="text-pink-800">Berbie doll</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Obcaecati, optio.
+            </p>
           </div>
         </div>
 
